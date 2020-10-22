f224f84fcd7faa0808582b69d91b10d66f8b72a2
mproemsey <mproemsey@esdemc.com>
2020-10-22 18:45:04 -0500

    convert borbin smash to she-ra

--- main_window.py
+++ main_window.py
@@ -23,11 +23,7 @@ class MainWindow(QMainWindow):
         self.haste_enabled = False
         self.raging_enabled = False
         self.power_attack_enabled = False
-        self.enlarged_enabled = False
-        self.surprise_accuracy_enabled = False
-        self.powerful_blow_enabled = False
         self.flanking_enabled = False
-        self.deadly_juggernaut_enabled = False
 
         # Connections to toggle state on check box click
         self.ui.two_handed_check_box.clicked.connect(self.two_handed_toggled)
@@ -35,14 +31,9 @@ class MainWindow(QMainWindow):
         self.ui.haste_check_box.clicked.connect(self.haste_toggled)
         self.ui.raging_check_box.clicked.connect(self.raging_toggled)
         self.ui.power_attack_check_box.clicked.connect(self.power_attack_toggled)
-        self.ui.enlarged_check_box.clicked.connect(self.enlarged_toggled)
-        self.ui.surprise_accuracy_check_box.clicked.connect(self.surprise_accuracy_toggled)
-        self.ui.powerful_blow_check_box.clicked.connect(self.powerful_blow_toggled)
         self.ui.flanking_bonus_check_box.clicked.connect(self.flanking_toggled)
-        self.ui.deadly_juggernaut_check_box.clicked.connect(self.deadly_juggernaut_toggled)
 
         # Auto update when spin box toggled
-        self.ui.kills_spin_box.valueChanged.connect(self.update_output)
         self.ui.num_hits_spin_box.valueChanged.connect(self.update_output)
 
         # Initialize output with initial settings
@@ -68,24 +59,8 @@ class MainWindow(QMainWindow):
         self.power_attack_enabled = state
         self.update_output()
 
-    def enlarged_toggled(self, state):
-        self.enlarged_enabled = state
-        self.update_output()
-
-    def surprise_accuracy_toggled(self, state):
-        self.surprise_accuracy_enabled = state
-        self.update_output()
-
-    def powerful_blow_toggled(self, state):
-        self.powerful_blow_enabled = state
-        self.update_output()
-
     def flanking_toggled(self, state):
         self.flanking_enabled = state
-        self.update_output()
-
-    def deadly_juggernaut_toggled(self, state):
-        self.deadly_juggernaut_enabled = state
         self.update_output()
 
     def update_output(self):
@@ -93,8 +68,6 @@ class MainWindow(QMainWindow):
         strength = self.configuration['STR']
         if self.raging_enabled:
             strength += self.configuration['RAGE_STR_BONUS']
-        if self.enlarged_enabled:
-            strength += self.configuration['ENLARGE_STR_BONUS']
         effective_strength_bonus = int((strength - 10) / 2)
 
         # Calculate number of attacks
@@ -140,17 +113,8 @@ class MainWindow(QMainWindow):
         if self.power_attack_enabled:
             attack_bonus += self.configuration['POWER_ATTACK_ATTACK']
 
-        if self.enlarged_enabled:
-            attack_bonus += self.configuration['ENLARGE']
-
-        if self.surprise_accuracy_enabled:
-            attack_bonus += self.configuration['SURPRISE_ACCURACY']
-
         if self.flanking_enabled:
             attack_bonus += self.configuration['FLANKING']
-
-        if self.deadly_juggernaut_enabled:
-            attack_bonus += self.ui.kills_spin_box.value()
 
         return attack_bonus
 
@@ -162,10 +126,7 @@ class MainWindow(QMainWindow):
             strength_damage = int(strength_damage * self.configuration['TWO_HANDED_MULTI'])
         damage += strength_damage
 
-        if self.powerful_blow_enabled:
-            damage += self.configuration['POWERFUL_BLOW']
-
-        power_attack_damage = 6
+        power_attack_damage = self.configuration['POWER_ATTACK_DAMAGE']
         if self.two_handed_enabled:
             power_attack_damage = int(power_attack_damage * self.configuration['TWO_HANDED_MULTI'])
         if self.power_attack_enabled:
@@ -174,12 +135,6 @@ class MainWindow(QMainWindow):
         if self.inspire_courage_enabled:
             damage += self.configuration['INSPIRE']
 
-        if self.deadly_juggernaut_enabled:
-            damage += self.ui.kills_spin_box.value()
-
-        if self.enlarged_enabled:
-            die = self.configuration['ENLARGE_DAMAGE_DIE']
-        else:
-            die = self.configuration['DAMAGE_DIE']
+        die = self.configuration['DAMAGE_DIE']
 
         return die, damage
